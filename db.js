7853461c3873caa0fb84ced5bc48d91fab60ff01
Fazil <fazil721655@gmail.com>
2025-11-26 12:29:04 +0530

    Add files via upload

---
+++ db.js
@@ -0,0 +1,17 @@
+// config/db.js
+import mongoose from "mongoose";
+
+
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log("Database Connection Error:", error.message);
+    process.exit(1); // Stop the app if DB fails
+  }
+};
+
+export default connectDB;
